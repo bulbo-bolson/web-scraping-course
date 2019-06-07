242fe9227aa452ae7582f93a1cd3e586e524de20
Curro <>
2019-06-07 10:21:40 +0200

    parseo todas las baterias

--- ejercicios/music_scraping.py
+++ ejercicios/music_scraping.py
@@ -1,18 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
-import pprint
 
 
-def get_results_page_data(results_url):
+def get_results_page_data(results_url, url_params = {}):
     page = {}
-    soup = BeautifulSoup(requests.get(results_url).text, 'html.parser')
+    print("[debug] parseando: " + results_url + str(url_params))
+    soup = BeautifulSoup(requests.get(results_url, params=url_params).text, 'html.parser')
     articles_container = soup.find(id='defaultResultPage')
     page['drum_link_list'] = []
     for article in articles_container.find_all('div', class_='list-view'):
         page['drum_link_list'].append(article.find('a', class_='article-link').attrs['href'])
     last_next_button = soup.find(id='resultPageNavigation').find_all(class_='rs-btn')[-1]
     page['last_next_page'] = last_next_button.get_text()
-    print("[debug] nº de links en este result: " + str(len(page['drum_link_list'])))
+    print("[debug] nº de links en este result set: " + str(len(page['drum_link_list'])))
     return page
 
 
@@ -24,13 +24,17 @@ def get_drumset_data(drum_url):
     return drum
     
 
-
 if __name__ == "__main__":
     results_url = "https://www.thomann.de/es/sets_de_bateria.html"
-    page = get_results_page_data(results_url)
-    for drum_link in page['drum_link_list']:
-        drum = get_drumset_data(drum_link)
-        print(drum)
-
+    RESULT_LIMIT = 25
+    page_1 = get_results_page_data(results_url)
+    counter = 1
+    while counter <= int(page_1['last_next_page']):
+        next_page_params = {'pg' : counter, 'ls' : RESULT_LIMIT}
+        next_page = get_results_page_data(results_url, url_params=next_page_params)
+        for drum_link in next_page['drum_link_list']:
+            drum = get_drumset_data(drum_link)
+            print(drum)
+        counter += 1
             
         
